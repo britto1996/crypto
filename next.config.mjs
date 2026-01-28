3b37557a7a4f8c9c89b214a12aad2a2b898daecb
britto1996 <btm15037@gmail.com>
2026-01-28 16:12:46 +0400

    Deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/crypto',
+    assetPrefix: '/crypto',
 };
 
 export default nextConfig;
